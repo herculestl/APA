31a45b0b3bb7626a5af6381a87de8773393d8e68
herculestl <37196195+herculestl@users.noreply.github.com>
2019-03-07 14:55:23 -0300

    Add files via upload

---
+++ heap.cpp
@@ -0,0 +1,41 @@
+#include <iostream> 
+using namespace std; 
+  
+void heapsort(int a[], int n) {
+   int i = n / 2, pai, filho, t;
+   while(true) {
+      if (i > 0) {
+          i--;
+          t = a[i];
+      } else {
+          n--;
+          if (n == 0) return;
+          t = a[n];
+          a[n] = a[0];
+      }
+      pai = i;
+      filho = i * 2 + 1;
+      while (filho < n) {
+          if ((filho + 1 < n)  &&  (a[filho + 1] > a[filho]))
+              filho++;
+          if (a[filho] > t) {
+             a[pai] = a[filho];
+             pai = filho;
+             filho = pai * 2 + 1;
+          } else {
+             break;
+          }
+      }
+      a[pai] = t;
+   }
+}
+  int main() 
+{ 
+    int a[] = {12, 11, 13, 5, 6, 7}; 
+    int n = sizeof(a)/sizeof(a[0]); 
+  
+    heapsort(a, n); 
+  
+    cout << "Sorted array is \n"; 
+    cout << a, n; 
+}
